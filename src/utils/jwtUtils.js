4a3e0b6442969b91eb01a6c627e12bf09c2962f3
Shiva Teja Nam <shivatejanam@Shivas-MacBook-Air.local>
2023-11-06 17:31:01 +0530

    google

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -2,6 +2,6 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
 exports.generateToken = (userId) => {
-  const token = jwt.sign({ sub: userId }, config.secretKey, { expiresIn: '1h' });
+  const token = jwt.sign({ sub: userId }, config.jwtsecretkey, { expiresIn: '1h' });
   return token;
 };
