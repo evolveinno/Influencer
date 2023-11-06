58dec4ba3036d829bdd59dca179959e7d131ffbc
Shiva Teja Nam <shivatejanam@Shivas-MacBook-Air.local>
2023-11-06 14:49:09 +0530

    Backend setup

--- src/app.js
+++ src/app.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const mongoSanitize = require('express-mongo-sanitize');
-const compression = require('compression');
-const cors = require('cors');
-const httpStatus = require('http-status');
-const config = require('./config/config');
-const morgan = require('./config/morgan');
-const { authLimiter } = require('./middlewares/rateLimiter');
-const routes = require('./routes/v1');
-const { errorConverter, errorHandler } = require('./middlewares/error');
-const ApiError = require('./utils/ApiError');
+const express = require("express");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const compression = require("compression");
+const cors = require("cors");
+const httpStatus = require("http-status");
+const config = require("./config/config");
+const morgan = require("./config/morgan");
+const { authLimiter } = require("./middlewares/rateLimiter");
+const routes = require("./routes/v1");
+const { errorConverter, errorHandler } = require("./middlewares/error");
+const ApiError = require("./utils/ApiError");
+const cookie = require("cookie-parser");
 
 const app = express();
 
-if (config.env !== 'test') {
+if (config.env !== "test") {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
 
 // set security HTTP headers
 app.use(helmet());
+
+app.use(cookie());
 
 // parse json request body
 app.use(express.json());
@@ -36,21 +39,25 @@ app.use(mongoSanitize());
 app.use(compression());
 
 // enable cors
-app.use(cors());
-app.options('*', cors());
-
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:8080"],
+  })
+);
+app.options("*", cors());
 
 // limit repeated failed requests to auth endpoints
-if (config.env === 'production') {
-  app.use('/v1/auth', authLimiter);
+if (config.env === "production") {
+  app.use("/v1/auth", authLimiter);
 }
 
 // v1 api routes
-app.use('/v1', routes);
+app.use("/v1", routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
 });
 
 // convert error to ApiError, if needed
